b37b6cd9b1bdf80aa37a7e4890e7c0a1955f37fa
inaladevi <106736232+inaladevi@users.noreply.github.com>
2026-03-16 01:03:40 +0530

    Add navigation tab switching for home and menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import loadHome from "./home.js";
+import loadMenu from "./menu.js";
 
 function clearContent() {
   const content = document.querySelector("#content");
   content.innerHTML = "";
 }
+
+const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+
+homeBtn.addEventListener("click", () => {
+  clearContent();
+  loadHome();
+});
+
+menuBtn.addEventListener("click", () => {
+  clearContent();
+  loadMenu();
+});
 
 clearContent();
 loadHome();
